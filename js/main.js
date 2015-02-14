7f532b8b136c3df04dcceb6bbe14ffdd8a072ff5
Aansh Kapadia <AanshKapadia@gmail.com>
2015-02-14 01:08:13 -0500

    Added Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
-
 $(document).ready(function () {
 
 	//fullpage scrolling
     $('#fullpage').fullpage({
-    	scrollingSpeed: 500,
-    	paddingBottom: '3em'
-    	// anchors: anchors,
-    	// animateAnchor: true
-    	// onLeave: function(index, nextIndex, direction)
-    	// {
-     //        var difference = Math.abs(nextIndex - index);
-     //        if(difference >= 2){
-     //        	$( ".section" ).fadeOut(300, function() {}).delay(700);
-     //        	$( ".section" ).fadeIn(400, function() {});
-     //        }
-     //        else {
-     //        	// uncomment for fade when scrolling bordering projects
-     //        	// $( ".section:eq(" + (index-1) + ")" ).fadeTo(1250, 0).delay(250);
-     //        	// $( ".section:eq(" + (index-1) + ")" ).fadeTo("fast", 1);
-     //        }
-     //    }
+    	scrollingSpeed: 600
+    	// paddingBottom: '3em'
+    	// autoScrolling: false
     });
 
 });
