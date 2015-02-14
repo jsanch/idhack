e703ab896a87a52f8915777c9092ed33673bdafa
George Brown <gbrown3593@gmail.com>
2015-02-14 00:02:10 -0500

    working on getting data; need to start working it into d3 asap

--- js/Model.js
+++ js/Model.js
@@ -1,12 +1,17 @@
 var Model = {
 	bubbleData:null,
+	allData:null,
+	popData:null
 
 	initData: function(callback) {
 
 		console.log("initData");
 	    Model.bubbleData = null;
 
-	    // LOAD ALL WE NEED 
+	    // LOAD ALL WE NEED
+	    $.get("https://communities.socrata.com/resource/rd25-4b5p.json?$$app_token=qqWOL1eWwrOBiLADaJ0cKz1j5", function( data ) {
+        	allData = data;
+        }); 
 		bubbleData =  Model.loadBubbleData(); 
 
 		callback.call(window, Model.bubbleData);
@@ -18,6 +23,14 @@ var Model = {
 		/// GEORGE GOES HERE.
 		/// 
 		console.log("load the data");
+	}
+
+	loadPopData: function (popDate) {
+		date = popDate;
+		url = "http://api.population.io/1.0/population/Tanzania/" + date + "2014-12-1/?format=json"; 
+		$.get(url, function(data) {
+			popData = data;
+		}
 	}
 
 
