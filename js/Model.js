bd243e060775c87b718455dc172d06a521515e7a
jsanch <jaime.sanchez.arias@gmail.com>
2015-02-14 01:05:10 -0500

    fixed atrocious mistakes made by george brown

--- js/Model.js
+++ js/Model.js
@@ -12,10 +12,16 @@ var Model = {
 	    // LOAD ALL WE NEED
 	    $.get("https://communities.socrata.com/resource/rd25-4b5p.json?$$app_token=qqWOL1eWwrOBiLADaJ0cKz1j5", function( data ) {
         	schoolData = data;
-        }); 
-		bubbleData =  Model.loadBubbleData(); 
+        	console.log(schoolData);
+        
+		    console.log("hello");
+			bubbleData =  Model.getBubbleData(); 
 
-		callback.call(window, Model.bubbleData);
+			callback.call(window, Model.bubbleData);
+
+        }); 
+
+
 
 	},
 
@@ -24,29 +30,36 @@ var Model = {
 		/// GEORGE GOES HERE.
 		/// 
 		console.log("load the data");
-	}
+	},
 
 	loadPopData: function (popDate) {
 		date = popDate;
 		url = "http://api.population.io/1.0/population/Tanzania/" + date + "2014-12-1/?format=json"; 
 		$.get(url, function(data) {
 			popData = data;
-		}
-	}
+		});
+	},
 
 	loadWaterData: function () {
-		$.get("https://communities.socrata.com/resource/npaw-5hb4.json?$$app_token=qqWOL1eWwrOBiLADaJ0cKz1j5", function(data)) {
+		$.get("https://communities.socrata.com/resource/npaw-5hb4.json?$$app_token=qqWOL1eWwrOBiLADaJ0cKz1j5", function(data) {
 			waterData = data;
-		}
-	}
+		});
+	},
 
 	aggregateRegionSchoolData: function () {
 		regions = {}
 
-		for(i in schoolData) {
+		// for(i in schoolData) {
 
-		}
+		// }
+	},
+
+	getBubbleData: function() {
+		console.log(Model.schoolData);
+
 	}
+
+
 
 
 }
